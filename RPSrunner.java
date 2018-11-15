e06f090c59b564eb99b1cf6813ee79d75234753b
Jared Miller <jaredmil@umich.edu>
2018-11-15 16:29:58 -0500

    Add more comprehensive comments

--- RPSrunner.java
+++ RPSrunner.java
@@ -4,44 +4,57 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class RPSrunner {
-
+	
 	public static void main(String[] args) {
 		RPSclass game = new RPSclass();
-
+		//create lists to hold each players' moves
 		ArrayList<Integer> player = new ArrayList<Integer>();
 		ArrayList<Integer> computer = new ArrayList<Integer>();
 		
+		//create scanner in console
 		Scanner input = new Scanner(System.in);
 		
+		//save name of user
 		String name = game.getReady(input);
 		
+		//create array to track [user wins, computer wins, ties]
 		int[] score = new int[3];
 		for(int i=0; i<3; i++) score[i] = 0;
 		
 		boolean willPlay = true;
 		int round = 1;
-		boolean doIt = true;
-		while(willPlay){
-			if(doIt){
+		boolean makeMove = true;
+		
+		//main loop for each turn
+		while(willPlay) {
+			if(makeMove) {
+				//initiate another round
 				computer = game.makeCMove(player, computer, round);
 				System.out.println("\nRock, Paper, Scissors, SHOOT!");
 			}
-			if(input.hasNext()){
+			if(input.hasNext()) {
+				//take in user command
 				String move = input.next();
-				if(move.charAt(0)=='E') break;
-				player = game.addMove(move, player);
-				score = game.findWinner(score, player, computer);
-				game.displayMovesAndScore(round, name,score,player,computer);
-				round++;
-				doIt = true;
-			} else{
+				
+				if(move.charAt(0) != 'E') {
+					//get user's move and display round results
+					player = game.addMove(move, player);
+					score = game.findWinner(score, player, computer);
+					game.displayMovesAndScore(round, name,score,player,computer);
+					round++;
+					makeMove = true;
+				} else {
+					//break while loop & end game
+					willPlay = false;
+				}	
+			} else {
+				//user just pressed enter...prompt them to make a move
 				System.out.println("Please make your move...");
-				doIt = false;
+				makeMove = false;
 			}
 		}
 		
 		input.close();
 		System.out.println("Thanks for playing!");
 	}
-
 }
